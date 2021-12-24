a84cd47950ed60a0cbe09301399e35ddae88f3cd
Вадим <vraspopin@inbox.ru>
2021-12-24 17:56:44 +0300

    Network status check fully reworked

--- app/src/main/java/ru/wintrade/mvp/model/network/NetworkStatus.kt
+++ app/src/main/java/ru/wintrade/mvp/model/network/NetworkStatus.kt
@@ -4,6 +4,5 @@ import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 
 interface NetworkStatus {
-    fun isOnline(): Observable<Boolean>
     fun isOnlineSingle(): Single<Boolean>
 }
