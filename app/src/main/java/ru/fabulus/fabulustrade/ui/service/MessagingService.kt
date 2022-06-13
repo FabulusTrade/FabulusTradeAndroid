68813b550e67be30c426bed93579908901cc8939
Vadim <vraspopin@inbox.ru>
2022-06-13 17:40:51 +0300

    push icon color fix

--- app/src/main/java/ru/fabulus/fabulustrade/ui/service/MessagingService.kt
+++ app/src/main/java/ru/fabulus/fabulustrade/ui/service/MessagingService.kt
@@ -33,34 +33,41 @@ class MessagingService : FirebaseMessagingService(), IMessagingService {
         presenter.updateToken(token)
     }
 
-    override fun showNotification(title: Spanned, operationResultTitle: Spanned, body: String, id: Int) {
+    override fun showNotification(
+        title: Spanned,
+        operationResultTitle: Spanned,
+        body: String,
+        id: Int
+    ) {
         val intent = Intent(this, MainActivity::class.java)
 
         val pendingIntent = PendingIntent.getActivity(
-                this, 0, intent,
+            this, 0, intent,
             PendingIntent.FLAG_IMMUTABLE
         )
 
         val channelId = getString(R.string.default_notification_channel_id)
 
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_app_launcher_foreground)
-                .setColor(Color.RED)
-                .setContentTitle(title)
-                .setStyle(NotificationCompat.InboxStyle()
-                        .addLine(operationResultTitle)
-                        .addLine(body))
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.ic_app_launcher_foreground)
+            .setColor(0x00BCC1)
+            .setContentTitle(title)
+            .setStyle(
+                NotificationCompat.InboxStyle()
+                    .addLine(operationResultTitle)
+                    .addLine(body)
+            )
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
 
         val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                    channelId,
-                    "Fabulus Trade",
-                    NotificationManager.IMPORTANCE_DEFAULT
+                channelId,
+                "Fabulus Trade",
+                NotificationManager.IMPORTANCE_DEFAULT
             )
             notificationManager.createNotificationChannel(channel)
         }
